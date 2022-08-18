911943b38f97547c9dd93a79fddebfacf2def0d7
Laureana Lupinacci <lalilupinacci@gmail.com>
2022-08-17 22:17:55 -0300

    Primeras correcciones

---
+++ js/stock.js
@@ -0,0 +1,29 @@
+let stockProductos = [
+    {id: 1, nombre: "Manga Shoujo", tipo: "Manga", desc: "Un Manga para purificar tu alma", precio: 1200, img: '/img/MangaShoujo.jpg'},
+    {id: 2, nombre: "Manga BL", tipo: "Manga", desc: "Un Manga para sucumbir al pecado", precio: 1200, img: '/img/MangaBL.jpg'},
+    {id: 3, nombre: "Manga GL", tipo: "Manga", desc: "Un Manga para sucumbir al pecado, pero en menor medida", precio: 1200, img: '/img/MangaGL.jpg'},
+    {id: 4, nombre: "Manga Shonen", tipo: "Manga", desc: "Un Manga para antisociales con necesidad de acción (de la sana)", precio: 1200, img: '/img/MangaShonen.jpg'},
+    {id: 5, nombre: "Manga Josei", tipo: "Manga", desc: "Un Manga para un puerco decente adulto", precio: 1200, img: '/img/MangaJosei.jpg'},
+    {id: 6, nombre: "Manga Seinen", tipo: "Manga", desc: "Un Manga para excéntricos", precio: 1200, img: '/img/MangaSeinen.png'},
+    {id: 7, nombre: "Libro Romance", tipo: "Libro", desc: "Un Libro para los solterones de la Familia", precio: 3500, img: '/img/LibroRom.jpg'},
+    {id: 8, nombre: "Libro Fantasía", tipo: "Libro", desc: "Un Libro para escapar de la realidad", precio: 3500, img: '/img/LibroFant.jpg'},
+    {id: 9, nombre: "Libro Histórico", tipo: "Libro", desc: "Un Libro para viajar en el tiempo", precio: 3500, img: '/img/LibroHist.png'},
+    {id: 10, nombre: "Libro Clásico", tipo: "Libro", desc: "Un Libro para sentirse culto", precio: 3500, img: '/img/LibroClas.jpg'},
+    {id: 11, nombre: "Libro Infantil", tipo: "Libro", desc: "Un Libro para el inocente de la Familia", precio: 3000, img: '/img/LibroInfant.jpg'},
+    {id: 12, nombre: "Cómic", tipo: "Cómic", desc: "Un Cómic a elección", precio: 4000, img: '/img/Comic.jpg'},
+    {id: 13, nombre: "Figura BNHA", tipo: "Figura", desc: "Figura de BNHA a elección", precio: 6500, img: '/img/FigBNHA.webp'},
+    {id: 14, nombre: "Figura Demon Slayer", tipo: "Figura", desc: "Figura de Demon Slayer a elección", precio: 6500, img: '/img/FigDS.webp'},
+    {id: 15, nombre: "Figura AOT", tipo: "Figura", desc: "Figura de AOT a elección", precio: 6500, img: '/img/FigAOT.jpg'},
+    {id: 16, nombre: "Figura Jujutsu Kaisen", tipo: "Figura", desc: "Figura de JJK a elección", precio: 6500, img: '/img/FigJJK.jpg'},
+    {id: 17, nombre: "Figura Naruto", tipo: "Figura", desc: "Figura de Naruto a elección", precio: 6500, img: '/img/FigNaruto.jpg'},
+    {id: 18, nombre: "Figura Hunter x Hunter", tipo: "Figura", desc: "Figura de HxH a elección", precio: 6500, img: '/img/FigHxH.jpg'},
+    {id: 19, nombre: "Peluche BNHA", tipo: "Peluche", desc: "Peluche de BNHA a elección", precio: 1000, img: '/img/PelBNHA.jpg'},
+    {id: 20, nombre: "Peluche Demon Slayer", tipo: "Peluche", desc: "Peluche de DS a elección", precio: 1000, img: '/img/PelDS.jpg'},
+    {id: 21, nombre: "Peluche Naruto", tipo: "Peluche", desc: "Peluche de Naruto a elección", precio: 1000, img: '/img/PelNaruto.jpg'},
+    {id: 22, nombre: "Peluche Kamisama Hajimemashita", tipo: "Peluche", desc: "Peluche de KH a elección", precio: 1000, img: '/img/PelKH.png'},
+    {id: 23, nombre: "Peluche Jujutsu Kaisen", tipo: "Peluche", desc: "Peluche de JJK a elección", precio: 1000, img: '/img/PelJJK.jpg'},
+    {id: 24, nombre: "Peluche Personalizado", tipo: "Peluche", desc: "Peluche de cualquier otro anime que no se encuentre en los otros productos", precio: 1500, img: '/img/PelX.jpg'},
+]
+
+
+
